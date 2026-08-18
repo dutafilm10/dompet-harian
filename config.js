8cdb8c09c5650278a0f1b47b7073f47821e9ecc8
dutafilm10 <dutafilm10@gmail.com>
2026-08-18 22:56:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
 window.SUPABASE_URL="https://plxrbmblhgzcpdikuwue.supabase.co";
-window.SUPABASE_PUBLISHABLE_KEY="sb_publishable_dwnKgdeFwU05Gr23s1F_KQ_U9dUcr5v
-";
+window.SUPABASE_PUBLISHABLE_KEY="sb_publishable_dwnKgdeFwU05Gr23s1F_KQ_U9dUcr5v";
